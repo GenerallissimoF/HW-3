f78640c8d10d425c037518c80f3249eefbd7b547
GenerallissimoF <95148081+GenerallissimoF@users.noreply.github.com>
2021-12-13 23:07:54 +0300

    Initial Commit

---
+++ HW 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 3
+//
+//  Created by Ivan Adoniev on 13.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
